add46a2572381d00c382036254a527ed664e4be5
Josh Lyall-Grant <LYA202002060@longroad.ac.uk>
2021-01-27 11:46:42 +0000

    Upload work changes

---
+++ Hokemon/Class.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hokemon
+{
+    class Hokemon
+    {
+        // Hokemon Attributes.
+        private string name;
+        private int max_health;
+        private int health;
+        private int attack;
+        private int defense;
+        private int speed;
+
+        // Below is my CONSTRUCTOR method for the Hokemon.
+        public Hokemon()
+        {
+            provide_name();
+            max_health = random_int_generator(10, 100);
+            health = max_health;
+            attack = random_int_generator(10, 100);
+            speed = random_int_generator(10, 100);
+            defense = random_int_generator(10, 100);
+
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
+
+        public void get_stats()
+        {
+            Console.WriteLine("\n##########################");
+            Console.WriteLine(String.Format("Name: {0,-10}", name));
+            Console.WriteLine(String.Format("Health: {0}/{1,-10}", health, max_health));
+            Console.WriteLine(String.Format("Attack: {0,-10}", attack));
+            Console.WriteLine(String.Format("Speed: {0,-10}", speed));
+            Console.WriteLine(String.Format("Defense: {0,-10}", defense));
+            Console.WriteLine("##########################");
+        }
+
+        public void provide_name()
+        {
+            Console.WriteLine("Enter a name for your Hokemon: ");
+            name = Console.ReadLine();
+        }
+
+        public int random_int_generator(int min_value, int max_value) // PARAMETERS
+        {
+            Random rnd = new Random();
+
+            int random_value;
+
+            random_value = rnd.Next(min_value, max_value);
+
+            return random_value;
+        }
+
+        public int attack_calculator()
+        {
+            int attack_value;
+            Random rnd = new Random();
+
+            attack_value = (attack * speed) / 2 * rnd.Next(1, 2);
+
+            return attack_value;
+        }
+
+        public int defense_calculator()
+        {
+            int defense_value;
+            Random rnd = new Random();
+
+            defense_value = (defense * speed) / 2 * rnd.Next(1, 3);
+
+            return defense_value;
+        }
+    }
+}
---
+++ Hokemon/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Hokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to HokeWorld - Home of all Hokemon!");
+
+            // INSTANTIATION of new Hokemon.
+
+            Hokemon hoke01 = new Hokemon(); // INSTANTIATE new Hokemon object referred to as hoke01.name
+            hoke01.get_stats();
+            Console.WriteLine("Attack Value is : {0}", hoke01.attack_calculator());
+            Console.WriteLine("Defense Value is : {0}", hoke01.defense_calculator());
+
+            Console.WriteLine("\n");
+
+            Hokemon hoke02 = new Hokemon();
+            hoke02.get_stats();
+            Console.WriteLine("Attack Value is : {0}", hoke02.attack_calculator());
+            Console.WriteLine("Defense Value is : {0}", hoke02.defense_calculator());
+
+            Console.WriteLine("\n");
+
+            Battle_Arena newBattleObject = new Battle_Arena();
+
+            newBattleObject.request_challenge(hoke02);
+
+            newBattleObject.the_battle(hoke02, hoke01);
+        }
+    }
+}
